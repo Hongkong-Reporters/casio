18c06121568d8d100493246e4d65aa2d5c51339f
ceilzcx <1758619238@qq.com>
2021-09-06 11:49:48 +0800

    修改多服务提供者测试

--- src/main/java/com/report/casio/test/starter/ProviderStarter2.java
+++ src/main/java/com/report/casio/test/starter/ProviderStarter2.java
@@ -1,6 +1,7 @@
 package com.report.casio.test.starter;
 
-public class Test {
+// 启动多个服务提供者时使用，启动前修改一下casio.yml的配置文件信息即可
+public class ProviderStarter2 {
 
     public static void main(String[] args) {
         new ProviderStarter().start();
